a584bba9ffd63493efccedccf343e4ae52497c10
sourabh <ksourabhsrivatsa@gmail.com>
2026-01-01 00:19:54 +0530

    VERSION 0.5 completed

--- src/pages/contactPage/contact.jsx
+++ src/pages/contactPage/contact.jsx
@@ -2,7 +2,6 @@ import ContactCard from "../../components/contactCard";
 import ProfilesSection from "../../components/profilesSection";
 
 const Contact = () => {
-  const { contactCard, profilesCard } = contactData;
   return (
     <div className="mt-6 py-4">
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 items-start">
